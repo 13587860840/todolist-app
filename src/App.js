d897d687bee7f57178d3eed31769e0ee28a5000b
13587860840 <123225898+13587860840@users.noreply.github.com>
2024-01-09 11:19:46 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div>
+    <div id='stars'></div>
+<div id='stars2'></div>
+<div id='stars3'></div>
+    <div className="App">
+      <TodoWrapper  />
+    </div>
+    </div>
+  );
+}
+
+export default App;
